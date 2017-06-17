504a96b330feac4caffcc45e0a711d98b52effd0
Karthic Rao <karthic@minio.io>
2017-06-17 07:35:11 +0530

    Radius of curvature calculated
    - Radius of curvature computed in meters

--- advance-lane.py
+++ advance-lane.py
@@ -324,3 +324,18 @@ plt.ylim(720, 0)
 plt.show()
 print(leftx_base)
 print(rightx_base)
+
+y_eval = np.max(ploty)
+
+# Define conversions in x and y from pixels space to meters
+ym_per_pix = 30/720 # meters per pixel in y dimension
+xm_per_pix = 3.7/700 # meters per pixel in x dimension
+
+# Fit new polynomials to x,y in world space
+left_fit_cr = np.polyfit(lefty*ym_per_pix, leftx*xm_per_pix, 2)
+right_fit_cr = np.polyfit(righty*ym_per_pix, rightx*xm_per_pix, 2)
+# Calculate the new radii of curvature
+left_curverad = ((1 + (2*left_fit_cr[0]*y_eval*ym_per_pix + left_fit_cr[1])**2)**1.5) / np.absolute(2*left_fit_cr[0])
+right_curverad = ((1 + (2*right_fit_cr[0]*y_eval*ym_per_pix + right_fit_cr[1])**2)**1.5) / np.absolute(2*right_fit_cr[0])
+# Now our radius of curvature is in meters
+print(left_curverad, 'm', right_curverad, 'm')
